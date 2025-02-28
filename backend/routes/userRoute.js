ec4cee33c9119fda93fc160b643b5ae04e1d1493
condua <phuc.phanphuc2002@hcmut.edu.vn>
2025-02-28 20:26:06 +0700

    new update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,71 +1,84 @@
-const express = require('express');
-const { registerUser, loginUser, logoutUser, getUserDetails, forgotPassword, resetPassword, updatePassword, updateProfile, getAllUsers, getSingleUser, updateUserRole, deleteUser, resetPasswordOtp } = require('../controllers/userController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
-const User = require('../models/userModel');
-const nodemailer = require('nodemailer')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUserDetails,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  updateProfile,
+  getAllUsers,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+  resetPasswordOtp,
+} = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+const User = require("../models/userModel");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
+router.route("/reset-password").post(resetPasswordOtp);
 
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
-router.route('/logout').get(logoutUser);
-router.route('/reset-password').post(resetPasswordOtp);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
-router.route('/me').get(isAuthenticatedUser, getUserDetails);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
-router.route('/password/forgot').post(forgotPassword);
-router.route('/password/reset/:token').put(resetPassword);
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+// router.route("/admin/users").get(getAllUsers);
 
-router.route('/password/update').put(isAuthenticatedUser, updatePassword);
-
-router.route('/me/update').put(isAuthenticatedUser, updateProfile);
-
-// router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
-router.route("/admin/users").get(getAllUsers);
-
-router.route("/admin/user/:id")
-    .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
-    .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
-    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
-
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'noreply.ewallet.hcmut@gmail.com',
-        pass: 'wsxweqdjcbdzenfn'
-    }
+  service: "Gmail",
+  auth: {
+    user: "noreply.ewallet.hcmut@gmail.com",
+    pass: "wsxweqdjcbdzenfn",
+  },
 });
 
-router.post('/forgot-password', async (req, res) => {
-        try {
-          const { email } = req.body;
-      
-          // Generate a random 6-digit OTP
-          const otp = Math.floor(100000 + Math.random() * 900000);
-      
-          // Save the OTP to the user's document in the database
-          const user = await User.findOne({ email });
-          if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-          }
-          user.resetPasswordOTP = otp;
-          await user.save();
-      
-          // Send the OTP to the user's email
-          const mailOptions = {
-              from: {
-                  name: 'Roced',
-                  address: 'noreply.ewallet.hcmut@gmail.com'
-              },
-              to: email,
-              subject: 'Reset Password OTP',
-              // text: `Your OTP for resetting password is: ${otp}`,
-              html:`
+router.post("/forgot-password", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Generate a random 6-digit OTP
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    // Save the OTP to the user's document in the database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    user.resetPasswordOTP = otp;
+    await user.save();
+
+    // Send the OTP to the user's email
+    const mailOptions = {
+      from: {
+        name: "Roced",
+        address: "noreply.ewallet.hcmut@gmail.com",
+      },
+      to: email,
+      subject: "Reset Password OTP",
+      // text: `Your OTP for resetting password is: ${otp}`,
+      html: `
               <!DOCTYPE html>
               <html>
               <head>
@@ -90,36 +103,35 @@ router.post('/forgot-password', async (req, res) => {
                   </div>
               </body>
               </html>
-          `
-          };
-      
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              console.error('Error sending email:', error);
-              res.status(500).json({ error: 'Error sending OTP email' });
-            } else {
-              console.log('Email sent:', info.response);
-              res.status(200).json({ message: 'OTP sent successfully', otp: otp });
-            }
-          });
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Server Error' });
-        }
-      });
+          `,
+    };
 
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({ error: "Error sending OTP email" });
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).json({ message: "OTP sent successfully", otp: otp });
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
-router.post('/send-welcome-email', async(req, res) =>{
+router.post("/send-welcome-email", async (req, res) => {
   try {
-        const { email, name } = req.body
-        const sendEmail = {
-          from: {
-            name: 'Roced',
-            address: 'noreply.ewallet.hcmut@gmail.com'
-          },
-          to: email,
-          subject: `Cảm ơn ${name} đã đăng ký thành công tài khoản tại Roced`,
-          html:`
+    const { email, name } = req.body;
+    const sendEmail = {
+      from: {
+        name: "Roced",
+        address: "noreply.ewallet.hcmut@gmail.com",
+      },
+      to: email,
+      subject: `Cảm ơn ${name} đã đăng ký thành công tài khoản tại Roced`,
+      html: `
           <!DOCTYPE html>
           <html>
           <head>
@@ -144,28 +156,21 @@ router.post('/send-welcome-email', async(req, res) =>{
               </div>
           </body>
           </html>
-          ` 
-        };
-        transporter.sendMail(sendEmail, (error, info) => 
-        {
-          if (error) 
-          {
-            console.error('Error sending email:', error);
-            res.status(500).json({ error: 'Error sending OTP email' });
-          } 
-          else 
-          {
-            console.log('Email sent:', info.response);
-            res.status(200).json({ message: 'OTP sent successfully', otp: otp });
-          }
-         }
-        )
-        res.status(200).json({ message: 'sent successfully' });
-
-  }catch(err){
+          `,
+    };
+    transporter.sendMail(sendEmail, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({ error: "Error sending OTP email" });
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).json({ message: "OTP sent successfully", otp: otp });
+      }
+    });
+    res.status(200).json({ message: "sent successfully" });
+  } catch (err) {
     console.error(err);
   }
-})    
-        
+});
 
 module.exports = router;
